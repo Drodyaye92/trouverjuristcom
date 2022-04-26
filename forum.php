390e2e11e75f1681f6054066251c1993185cbe48
Drodyaye92 <aichanana24@gmail.com>
2022-04-26 19:23:12 +0000

    premier commit

---
+++ forum.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="font-awesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="../font-awesome/css/all.css">
+    <link rel="stylesheet" href="../font-awesome/css/brands.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel=" stylesheet " href="css/flaticon.css ">
+    <link rel="stylesheet " href="css/style.css ">
+    <link rel="icon" href="images/Balance.jpg">
+    <title>Trouver mon juriste.com</title>
+</head>
+<body>
+    
+</body>
+</html>
