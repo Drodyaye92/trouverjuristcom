116490b706daf66c470e6227c967d9903bfa778d
Drodyaye92 <aichanana24@gmail.com>
2022-05-10 14:54:13 +0000

    commit du 10mai

--- forum.php
+++ forum.php
@@ -19,6 +19,51 @@
     <title>Trouver mon juriste.com</title>
 </head>
 <body>
+<header class="fixed-top">
+        <nav class="navbar  navbar-light bg-light">
+            <div class="container-fluid">
+                <label for="" class="numero">Trouvermonjurist.com</label>
+                <div class="om-social">
+                    <a href="#connexion.html"> <button class="btn btn-primary" type="submit">Connexion</button></a>
+                    <a href="#connexion.html"> <button class="btn btn-danger" type="submit">Inscription</button></a>
+
+                    <!-- <i class="fa fa-mobile-phone"></i> +226 73768124
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-youtube-play"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-pinterest-p"></i></a> -->
+                </div>
+
+            </div>
+        </nav>
+        <nav class="menu">
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn"> <i class="fa fa-bars"></i></label>
+
+            <ul>
+                <li>
+                    <a href="#">Accueil</a>
+                </li>
+                <li>
+                    <a href="service.html">Services</a>
+                </li>
+                <li>
+                    <a href="equipe.html">Equipe</a>
+                </li>
+                <li>
+                    <a href="programme.html">Programme</a>
+                </li>
+                <li>
+                    <a href="#">Forum</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+
+            </ul>
+        </nav>
+    </header>
     
 </body>
 </html>
